4584d658614836096f4600e64a4df732fe19fda6
Gareza <ajgareza@manila.equicom.com>
2025-03-26 08:56:24 +0800

    sample website vite

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import './App.css';
+
+function App() {
+    return <div>sample website</div>;
+}
+
+export default App;
--- webpack/rules/image.js
+++
@@ -1,9 +0,0 @@
-const PATHS = require('../paths');
-
-module.exports = ({ limit = 10000 } = {}) => ({
-  test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|otf|webp|avif)$/,
-  loader: 'url-loader',
-  options: { name: '[hash].[ext]', limit },
-  include: PATHS.app
-});
-
